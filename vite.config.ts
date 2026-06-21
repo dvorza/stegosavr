cb07f0ea5feb049021855ce7169066c4bbb572f7
Kir Dyk <navinynka@gmail.com>
2026-06-21 19:01:26 +0200

    NEW serve from root level

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-<<<<<<< HEAD
   base: "/",
-=======
-  base: "/stegosavr/",
   plugins: [react()],
->>>>>>> origin/main
 });
